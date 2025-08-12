55e68d7028fe36d8ddf2f0198f8eee97cd24e4e8
Lemnon95 <simonemille7@gmail.com>
2025-08-12 12:51:12 +0200

    feat: added colorama for menu

--- movie_tracker/menu.py
+++ movie_tracker/menu.py
@@ -1,6 +1,21 @@
+from colorama import Fore, Style, init
+
+
 def print_menu():
-    print("1 -- Insert a new movie")
-    print("2 -- Update your configuration file")
-    print("3 -- Update movie(s) with latest IMDb data")
-    print("4 -- Get recommendations")
-    print("5 -- Quit")
+    init(autoreset=True)  # Needed for Windows to reset colors automatically
+
+    print(f"\n{Fore.CYAN}{Style.BRIGHT}🎬  Movie Tracker Menu")
+    print(f"{Fore.CYAN}{'─' * 30}")
+
+    menu_items = [
+        ("1", "Insert a new movie"),
+        ("2", "Update your configuration file"),
+        ("3", "Update movie(s) with latest IMDb data"),
+        ("4", "Get recommendations"),
+        ("5", "Quit"),
+    ]
+
+    for key, desc in menu_items:
+        print(f"{Fore.YELLOW}{key} {Fore.WHITE}-- {desc}")
+
+    print(f"{Fore.CYAN}{'─' * 30}")
